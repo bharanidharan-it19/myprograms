eaaba0dee7ee58772decc0ee2738b6deab9186fc
bharanidharan-it19 <bharanidharan.it19@bitsathy,ac.in>
2019-10-10 15:10:25 +0530

    sixth commit

---
+++ prog36.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  char ch;
+  printf("\n Please Enter any character \n");
+  scanf("%c", &ch);
+  if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') )
+    printf("\n %c is an Alphabet", ch);
+  else
+    printf("\n %c is a Number", ch);
+   return 0;
+}
